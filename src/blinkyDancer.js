5eb8d981eebb04b98b4cd3c76649962226aa40fa
Zechariah Deckert-Holscher <zdeckert@gmail.com>
2020-03-31 11:11:14 -0700

    added sizing dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -20,7 +20,6 @@
 
 var MakeBlinkyDancer= function (top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
-
 };
 
 MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
