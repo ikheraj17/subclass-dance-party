c25f20c036e5275f3c419aa34197c187e7b7dc44
Imran Kheraj <ikheraj17@gmail.com>
2020-03-31 12:54:16 -0500

    Moved newDancer to be in src, added styling to CSS for new dancer

--- src/sizingDancer.js
+++ src/sizingDancer.js
@@ -1,12 +1,14 @@
 var MakeSizingDancer= function (top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="dancer" id="sizing"></span>');
 };
 
 MakeSizingDancer.prototype = Object.create(MakeDancer.prototype);
 MakeSizingDancer.prototype.constructor = MakeSizingDancer;
 
 MakeSizingDancer.prototype.step = function() {
-  MakeDancer.prototype.step.call(this); //invoked ''oldstep"
+  MakeDancer.prototype.step.call(this);
+  //this.$node.toggle(); //invoked ''oldstep"
 };
 
 MakeSizingDancer.prototype.addAnimation = function() {
